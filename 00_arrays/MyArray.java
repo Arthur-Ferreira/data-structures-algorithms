3567b3d49d7dca2bb0d298a57e8c808b93e4cec7
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-09-03 16:49:34 -0300

    Building my own Array, started methods get and push

---
+++ 00_arrays/MyArray.java
@@ -0,0 +1,31 @@
+/* Master the Coding Interview: Data Structures + Algorithms
+*
+*   Programa: Criando Meu Proprio Array
+*   Objetivo: 
+*
+*   Autor: Arthur Ferreira
+*/
+
+import java.util.ArrayList;
+
+public class MyArray {
+  private int length;
+  private ArrayList<Object> data;
+
+  public MyArray() {
+    this.length = 0;
+    this.data = new ArrayList<>();
+  }
+
+  public Object get(int index) {
+    return data.get(index);
+  }
+
+  public int push(Object item) {
+    data.add(item);
+    length++;
+    return length;
+  }
+
+
+}
