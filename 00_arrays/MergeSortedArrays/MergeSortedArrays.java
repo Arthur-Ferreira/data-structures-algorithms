2eb4f2c99db4b7732ee452325ea29f8130ff0680
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-09-05 17:27:41 -0300

    Add MergeSortedArrays class

---
+++ 00_arrays/MergeSortedArrays/MergeSortedArrays.java
@@ -0,0 +1,43 @@
+package MergeSortedArrays;
+
+/* Master the Coding Interview: Data Structures + Algorithms
+ *
+ *   Programa: Mesclar Arrays Ordenados
+ *   Objetivo:
+ *
+ *   Autor: Arthur Ferreira
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSortedArrays {
+
+  public List<Integer> mergeSortedArrays(int[] arr1, int[] arr2){
+    List<Integer> mergedArray = new ArrayList<>();
+    int i = 0, j = 0;
+
+    if(arr1.length == 0) {
+      for(int num : arr2) {
+        mergedArray.add(num);
+      }
+    }
+    if(arr2.length == 0) {
+      for(int num : arr1) {
+        mergedArray.add(num);
+      }
+    }
+
+    while(i < arr1.length || j < arr2.length) {
+      if (j >= arr2.length || (i < arr1.length && arr1[i] < arr2[j])) {
+        mergedArray.add(arr1[i]);
+        i++;
+      } else {
+        mergedArray.add(arr2[j]);
+        j++;
+      }
+    }
+
+    return mergedArray;
+  }
+}
